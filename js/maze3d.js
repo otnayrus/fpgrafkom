e518a4ace894bd0394533baf092a80d5a0be903d
Daiya Oomine <farrasrahmatullah@gmail.com>
2018-12-16 18:34:01 +0700

    hehe
    hehe

--- js/maze3d.js
+++ js/maze3d.js
@@ -2,26 +2,24 @@
 
 var width = window.innerWidth * 0.995;
 var height = window.innerHeight * 0.995;
-var canvasContainer = document.getElementById("canvasContainer");
+var canvasContainer = document.getElementById("canvas");
 var renderer, camera, scene;
 var input, miniMap, CameraHelper;
 var map = new Array();
 var running = true;
 var elem = document.documentElement;
-
+var level = 1;
 
 window.onload = function() {
         initializeEngine();
-        var level = 1; // Get parameter
         loadLevel(1);
     };
 
 
 
-
-function loadLevel(level) {
+function loadLevel(maplevel) {
         var ajax = new XMLHttpRequest();
-        ajax.open("GET", "maps/maze3d-" + level + ".json", true);
+        ajax.open("GET", "maps/level" + maplevel + ".json", true);
         ajax.onreadystatechange = function() {
             if (ajax.readyState == 4) {
                 map = JSON.parse(ajax.responseText);
@@ -53,7 +51,7 @@ function initializeEngine() {
         camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
         camera.position.y = 50;
 
-        document.getElementById("canvasContainer").appendChild(renderer.domElement);
+        document.getElementById("canvas").appendChild(renderer.domElement);
 
         input = new Demonixis.Input();
         levelHelper = new Demonixis.GameHelper.LevelHelper();
@@ -82,10 +80,6 @@ function initializeEngine() {
         messageContainer.style.left = (window.innerWidth / 2 - messageContainer.offsetWidth / 2) + "px";
         messageContainer.style.top = (window.innerHeight / 2 - messageContainer.offsetHeight / 2) + "px";
 
-
-
-
-
         window.addEventListener("click", function() {
             openFullscreen();
             document.body.removeChild(messageContainer);
@@ -99,6 +93,205 @@ function initializeEngine() {
     }
 
 
+ function initializeScene() {
+        //miniMap = new Demonixis.Gui.MiniMap(map[0].length, map.length, "canvasContainer");
+        //miniMap.create();
+
+        var loader = new THREE.TextureLoader();
+        var platformWidth = map[0].length * 100;
+        var platformHeight = map.length * 100;
+
+        var floorGeometry = new THREE.BoxGeometry(platformWidth, 5, platformHeight);
+        var ground = new THREE.Mesh(floorGeometry, new THREE.MeshPhongMaterial());
+
+        repeatTexture(ground.material.map, 2);
+
+        ground.position.set(-50, 1, -50);
+        scene.add(ground);
+
+        var topMesh = new THREE.Mesh(floorGeometry, new THREE.MeshPhongMaterial());
+
+        repeatTexture(topMesh.material.map, 16);
+
+        topMesh.position.set(-50, 100, -50);
+        scene.add(topMesh);
+
+
+        var position = { 
+            x: 0, 
+            y: 0, 
+            z: 0 
+        };
+
+        var wallGeometry = new THREE.BoxGeometry(100, 100, 100);
+        var wallMaterial = new THREE.MeshPhongMaterial();
+
+        repeatTexture(wallMaterial.map, 2);
+
+        // Map generation
+        for (var y = 0, ly = map.length; y < ly; y++) {
+            for (var x = 0, lx = map[x].length; x < lx; x++) {
+                position.x = -platformWidth / 2 + size.x * x;
+                position.y = 50;
+                position.z = -platformHeight / 2 + size.z * y;
+
+                if (x == 0 && y == 0) {
+                    cameraHelper.origin.x = position.x;
+                    cameraHelper.origin.y = position.y;
+                    cameraHelper.origin.z = position.z;
+                }
+
+                if (map[y][x] > 1) {
+                    var wall3D = new THREE.Mesh(wallGeometry, wallMaterial);
+                    wall3D.position.set(position.x, position.y, position.z);
+                    scene.add(wall3D);
+                }
+
+                if (map[y][x] === "D") {
+                    camera.position.set(position.x, position.y, position.z);
+                    cameraHelper.origin.position.x = position.x;
+                    cameraHelper.origin.position.y = position.y;
+                    cameraHelper.origin.position.z = position.z;
+                    cameraHelper.origin.position.mapX = x;
+                    cameraHelper.origin.position.mapY = y;
+                    cameraHelper.origin.position.mapZ = 0;
+                }
+
+                miniMap.draw(x, y, map[y][x]);
+            }
+        }
+
+        // Lights
+        var directionalLight = new THREE.HemisphereLight(0x192F3F, 0x28343A, 2);
+        directionalLight.position.set(1, 1, 0);
+        scene.add(directionalLight);
+    }
+
+    function update() {
+        if (input.keys.up) {
+            moveCamera("up");
+        } else if (input.keys.down) {
+            moveCamera("down");
+        }
+
+        if (input.keys.left) {
+            moveCamera("left");
+        } else if (input.keys.right) {
+            moveCamera("right");
+        }
+
+    }
+
+    function draw() {
+        renderer.render(scene, camera);
+    }
+
+
+    function moveCamera(direction, delta) {
+        var collides = false;
+        var position = {
+            x: camera.position.x,
+            z: camera.position.z
+        };
+        var rotation = camera.rotation.y;
+        var offset = 50;
+
+        var moveParamaters = {
+            translation: (typeof delta != "undefined") ? delta.translation : cameraHelper.translation,
+            rotation: (typeof delta != "undefined") ? delta.rotation : cameraHelper.rotation
+        };
+
+        switch (direction) {
+            case "up":
+                position.x -= Math.sin(-camera.rotation.y) * -moveParamaters.translation;
+                position.z -= Math.cos(-camera.rotation.y) * moveParamaters.translation;
+                break;
+            case "down":
+                position.x -= Math.sin(camera.rotation.y) * -moveParamaters.translation;
+                position.z += Math.cos(camera.rotation.y) * moveParamaters.translation;
+                break;
+            case "left":
+                rotation += moveParamaters.rotation;
+                break;
+            case "right":
+                rotation -= moveParamaters.rotation;
+                break;
+        }
+
+        var tx = Math.abs(Math.floor(((cameraHelper.origin.x + (camera.position.x * -1)) / 100)));
+        var ty = Math.abs(Math.floor(((cameraHelper.origin.z + (camera.position.z * -1)) / 100)));
+
+        var newTx = Math.abs(Math.floor(((cameraHelper.origin.x + (position.x * -1) + (offset)) / 100)));
+        var newTy = Math.abs(Math.floor(((cameraHelper.origin.z + (position.z * -1) + (offset)) / 100)));
+
+        // Stay on the map
+        if (newTx >= map[0].length) {
+            newTx = map[0].length;
+        }
+        if (newTx < 0) {
+            newTx = 0;
+        }
+        if (newTy >= map.length) {
+            newTy = map.length;
+        }
+        if (newTy < 0) {
+            newTy = 0;
+        }
+
+        if (map[newTy][newTx] != 1 && !isNaN(map[newTy][newTx])) {
+            collides = true;
+        } else if (map[newTy][newTx] == "A") {
+            running = false;
+        }
+
+        if (collides == false) {
+            camera.rotation.y = rotation;
+            camera.position.x = position.x;
+            camera.position.z = position.z;
+
+            miniMap.update({
+                x: newTx,
+                y: newTy
+            });
+        } else {
+            document.getElementById("bumpSound").play();
+        }
+    }
+
+
+    function mainLoop(time) {
+        if (running) {
+            update();
+            draw();
+            window.requestAnimationFrame(mainLoop, renderer.domElement);
+        } else {
+            endScreen();
+        }
+    }
+
+    function endScreen() {
+        level=level+1;
+
+        if (level==2) {
+            alert("Terima Kasih udah mainin FP kelompok kami");
+        } else {
+            for (var i = 0, l = scene.children.length; i < l; i++) {
+                scene.remove(scene.children[i]);
+            }
+            renderer.clear();
+            scene = new THREE.Scene();
+            loadLevel(level);
+            running = true;
+        }
+    }
+
+    function repeatTexture(texture, size) {
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.repeat.x = size;
+        texture.repeat.y = size;
+        return texture;
+    }
 
 
 
